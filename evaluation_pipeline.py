1e65b41ba9517949f1bdc29e3d75802ec5663172
Terry Cheng <qwe0940182@gmail.com>
2023-06-16 17:17:40 +0800

    (i)Upload evaluation pipeline and utils.

---
+++ evaluation_pipeline.py
@@ -0,0 +1,257 @@
+### Necessary library
+#general_frame_work
+import pandas as pd
+import numpy as np
+#evaluation
+from .utils import timer
+from sklearn.model_selection import GridSearchCV, StratifiedKFold
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.metrics import  confusion_matrix, roc_auc_score, accuracy_score, f1_score, recall_score, precision_score
+from imblearn.metrics import geometric_mean_score
+#store model
+import pickle
+from typing import Literal
+
+
+class Train_model():
+    """
+    # Parameters
+    train_data : pd.DataFrame, default = None
+        The datasets that used to buildi a classification model and validate.
+        NOTE: The target variable (or Y) of a given dataset should be placed at the last column.
+
+    classifier : any, default = None
+        A sklearn classifier that accepts the array-liked dataset.
+
+    parameters_dict : dict, default = None
+        A dictionary-liked parameter that used to tune the parameters of the given classifier.
+
+    resampler : any, default = None
+        A resampling method that is capable to fit a array-liked dataset
+
+    scaling : Literal, default = None
+        MinMaxScaler ('MinMax'), StandardScaler('Std) for scaling the data. If None, do not scaling.
+
+    kfold : int, default = 5
+        The number of folds that datset will be seperated in validation.
+    ---
+    # Artributes
+    evaluation_outcome : pd.DataFrame
+        The average value of stratified k-fold validation.
+
+    best_para : dict
+        The best set of parameters of classifier in the given parameter dict (or self.parameters_dict).
+    ---
+    # Example
+    >>> import pandas as pd
+    >>> import smote_variants as sv
+    >>> from sklearn.tree import DecisionTreeClassifier
+    >>> from validation_module import Train_model
+    >>> 
+    >>> data = pd.DataFrame([[1, 2, 1], [1, 0, 1], [10, 4, 0], [10, 0, 0], [10, 2, 0], [1, 4, 1], [10, 4, 0], [10, 4, 0], [10, 4, 0], [10, 4, 0]])
+
+    // Suppose the last column is the label of test dataset.
+    >>> X, Y = data.iloc[:, 0:-1], data.iloc[:, -1]
+    >>> Y.value_counts()
+    >>> 0.0    7
+        1.0    3
+
+    >>> classifier = DecisionTreeClassifier()
+    >>> resampler = sv.ROSE()
+    >>> 
+    >>> train_model = Train_model(
+        train_data = data, 
+        classifier = classifier,
+        resampler = resampler
+        kfold = 2)
+    >>> 
+    >>> train_model.evaluation()
+    >>> print(train_model.evaluation_outcome)
+           Accuracy  AUC  F1-score  Recall  Precision  G-mean
+        0       1.0  1.0       1.0     1.0        1.0     1.0
+    """
+
+    def __init__(
+        self,
+        train_data: pd.DataFrame = None,
+        classifier: any = None,
+        parameters_dict: dict = None,
+        resampler: any = None,
+        scaling: Literal['MinMax', 'Std', None] = None,
+        kfold: int = 5,
+         ):
+        # Parameters
+        self.train_data : pd.DataFrame = train_data
+        self.kfold : int = kfold
+        self.parameters_dict = parameters_dict
+        self.scaling : str = scaling
+        self.resampler = resampler
+        self.classifier = classifier
+        # Attributes
+        self.evaluation_outcome : pd.DataFrame = None
+        self.best_para :dict = None
+            
+
+    def get_preprocessed_data(self, data: pd.DataFrame):
+        '''
+        Given an imbalanced dataset and resampler, then return a processed balanced dataset.
+        '''
+        data = data.reset_index(drop = True)
+
+        if self.resampler == None:
+            return data.iloc[:, 0:-1], data.iloc[:, -1]
+        
+        # If the resampler is a k-NN based algorithm, and if the number of minority class instances is less that default k(5)
+        # , the k would be set to the number of minority class instances in the given dataset.
+        # if 'n_neighbors' in dir(self.resampler) and (min(data.Class.value_counts()[:]) < self.resampler.get_params()['n_neighbors']):
+        if 'n_neighbors' in dir(self.resampler) and (min(data.Class.value_counts()[:]) < self.resampler.n_neighbors):
+            self.resampler.set_params(**{'n_neighbors': min(data.Class.value_counts()[:]) - 1})
+        
+        # if 'k_neighbors' in dir(self.resampler) and (min(data.Class.value_counts()[:]) < self.resampler.get_params()['k_neighbors']):
+        if 'k_neighbors' in dir(self.resampler) and (min(data.Class.value_counts()[:]) < self.resampler.k_neighbors):
+            self.resampler.set_params(**{'k_neighbors': min(data.Class.value_counts()[:]) - 1})
+
+        b_X, b_Y = self.resampler.fit_resample(np.array(data.iloc[:, 0:-1]), np.array(data.iloc[:, -1]))
+        b_X = pd.DataFrame(b_X, columns = [str(x) for x in np.arange(b_X.shape[1])])
+        b_Y = pd.Series(b_Y, name = 'Class')
+        # print(id(b_X.shape))
+
+        return b_X, b_Y
+
+    def tune_para(self):
+        '''Given classifier and dict-liked parameters seed return 
+        the best parameter settings of classifier.
+        '''
+        grid_search = GridSearchCV(
+            estimator = self.classifier,
+            param_grid = self.parameters_dict,
+            cv = self.kfold,
+            n_jobs = -1,
+            scoring = 'roc_auc'
+            )
+        processed_data = self.get_preprocessed_data(self.train_data)
+        grid_search.fit(processed_data[0], processed_data[1])
+
+        self.best_para = grid_search.best_params_
+
+        del grid_search, processed_data
+
+    def training_model(self, data : pd.DataFrame,):
+        if self.parameters_dict != None:
+            model = self.classifier.set_params(**self.best_para)
+        else:
+            model = self.classifier
+
+        x, y = self.get_preprocessed_data(data)
+        fit_model = model.fit(x, y)
+        # self.shape = pd.concat((self.shape, y.value_counts()), axis = 1)
+        # x.columns = data.columns[0:-1]
+        
+        del x, y
+        return fit_model
+
+    def cross_validation(self):
+        # Initializing column of the dataset
+        variable_names = [str(x) for x in np.arange(self.train_data.shape[1] - 1)]
+        variable_names.append('Class')
+        self.train_data.columns = variable_names
+        
+        # Spliting dataset
+        kf = StratifiedKFold(n_splits = self.kfold, shuffle = True, random_state = 777)
+        train_list = []
+        test_list = []
+        # Get K-Folds index 
+        for train, test in kf.split(self.train_data.iloc[:,0:-1], self.train_data.iloc[:, -1]):
+            train_list.append(train)
+            test_list.append(test)
+
+        # Cross validation
+        metrics_list = ['Accuracy', 'AUC', 'F1-score', 'Recall', 'Precision', 'G-mean']
+        evl_outcome = np.zeros((self.kfold, len(metrics_list))) #K-folds, metrics = 6
+        for fold in range(self.kfold):
+            train_id = train_list[fold]
+            test_id = test_list[fold]
+            training = self.train_data.iloc[train_id, 0:-1]
+            testing = self.train_data.iloc[test_id, 0:-1]
+
+        # Standardization or Normalization
+            if self.scaling != None:
+                if self.scaling == 'MinMax':
+                    scaler = MinMaxScaler().fit(training)
+                elif self.scaling == 'Std':
+                    scaler = StandardScaler().fit(training)
+
+                training = pd.DataFrame(scaler.transform(training))
+                testing =  pd.DataFrame(scaler.transform(testing))
+                training['Class'] = self.train_data.iloc[train_id, -1].reset_index(drop = True)
+                testing['Class'] = self.train_data.iloc[test_id, -1].reset_index(drop = True)
+            else:
+                training['Class'] = self.train_data.iloc[train_id, -1]
+                testing['Class'] = self.train_data.iloc[test_id, -1]
+
+            ################################################################################################
+            # TODO : General form for setting the testset
+            # General Hint: Not all of the module named testset as a same way!(Maybe RegEx)
+            # if test_set in dir(self.resampler):
+            #     self.resampler.set_params(parameters = {'test_set' : testing})
+            # if isinstance(self.resampler, RDBH):
+            #     self.resampler.set_params(parameters_dict = {'x_test' : testing.iloc[:, 0:-1], 'y_test' : testing.iloc[:, -1]})
+            # if isinstance(self.resampler, ClusIBasedOversampling):
+            #     self.resampler.set_params(parameters = {'test_set' : testing})
+            # if testing == None:
+                # print('None testing')
+
+            model = self.training_model(data = training)
+            testing.columns = training.columns
+            pred = model.predict(testing.iloc[:, 0:-1])
+            true = testing.iloc[:, -1]
+
+            # TODO : Calculate evaluation metrics by array instead of for loop
+            for metric in range(len(metrics_list)):
+                evl_outcome[fold, metric] = self.get_metrics_value(metrics_list[metric], true, pred)
+
+        # self.train_data : pd.DataFrame = None
+        # self.classifier : any = None
+        # self.parameters_dict : dict = None
+        # self.resampler = None
+
+        return evl_outcome
+
+    def get_metrics_value(self, metrics, true, pred):
+
+        metrics_dict = {
+            'Accuracy' : accuracy_score(true, pred),
+            'AUC' : roc_auc_score(true, pred), 
+            'F1-score' : f1_score(true, pred, zero_division = 0),
+            'Recall' : recall_score(true, pred, zero_division = 0),
+            'Precision' : precision_score(true, pred, zero_division = 0), 
+            'G-mean' : geometric_mean_score(true, pred,),
+            }
+
+        if metrics in metrics_dict.keys():
+            return metrics_dict[metrics]
+        else:
+            raise Exception('Metric could not be found!')
+        
+    @timer
+    def evaluation(self):
+        '''Return the evaluation outcome of the given dataset based on the parameter settings
+        of the `Train_model` module.
+        '''
+
+        metrics_name =   ['Accuracy', 'AUC', 'F1-score', 'Recall', 'Precision', 'G-mean'] 
+        evl_table = np.zeros((1, len(metrics_name)))
+        if self.parameters_dict != None:
+            self.tune_para()
+            
+        table = self.cross_validation()
+        evl_table[0, :] = table.mean(axis = 0)
+        self.evaluation_outcome = pd.DataFrame(evl_table, columns = metrics_name)
+
+        return pd.DataFrame(evl_table, columns = metrics_name)
+    
+    def set_params(self, **parameters : dict):
+        for para_name in parameters.keys():
+            self.__dict__[para_name] = parameters[para_name]
+
+        # {'train_data': None, 'classifier': None, 'parameters_list': None, 'resampler': None}
---
+++ utils.py
@@ -0,0 +1,65 @@
+import pandas as pd 
+import numpy as np 
+import os
+import time
+
+__all__ =['import_dataset',
+          'read_data_list',
+          'timer']
+
+def import_dataset(path : str) -> pd.DataFrame:
+    '''
+    path + file name
+    ### Example
+    >>> path = './{DATASET NAME.EXT}'
+    >>> import_dataset(path)
+    >>> print(data.head(3))
+    ...       0     1     2    3     4     5     6 Class
+    ... 0  0.49  0.29  0.48  0.5  0.56  0.24  0.35     0
+    ... 1  0.07  0.40  0.48  0.5  0.54  0.35  0.44     0
+    ... 2  0.56  0.40  0.48  0.5  0.49  0.37  0.46     1
+    '''
+    path = path
+    dataset = pd.read_csv(path, header = 0, comment = '@', sep = ', ', engine = 'python')
+
+    name = [str(x) for x in np.arange(dataset.shape[1] - 1)]
+    name.append('Class')
+    # dataset = dataset.rename(columns = name, inplace = False)
+    dataset.columns = name
+    dataset['Class'].replace('negative', 0, inplace = True)
+    dataset['Class'].replace('positive', 1, inplace = True)
+    # for i in range(dataset.shape[0]):
+    #     if (dataset.iloc[i, -1] == ' negative') or (dataset.iloc[i, -1] == 'negative'):
+    #         dataset.iloc[i, -1] = 0
+    #     else:
+    #         dataset.iloc[i, -1] = 1
+
+    dataset['Class'] = dataset['Class'].astype("category")
+    del name
+    return dataset
+
+def read_data_list(path : str = None, fileExt : str = '.dat') -> list:
+
+    temp = os.listdir(path = path)
+    data_list = []
+
+    for i in temp:
+        a = os.path.splitext(i)
+        if a[-1] == fileExt:
+            data_list.append(a[-2])
+            
+    del a, temp        
+    return data_list
+
+def timer(func):
+    def wrapper(*args):
+        start = time.time()
+        result = func(*args)
+        gap = time.time() - start
+        out = [time.asctime(), int(gap  // 60), gap %60]
+        print('Process has been compeleted! \nSys time : {0:s} Exe time : {1:3d} mins {2:.4f} s'.format(*out))
+        # logging.debug('Process has been compeleted! \n Exe time : {0:3d} mins {1:.4f} s'.format(*out))
+        return result
+    return wrapper
+
+
